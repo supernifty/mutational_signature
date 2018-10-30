0a9e2fe4d5194767d45788da104120c53172e077
Peter Georgeson <peter@supernifty.org>
2018-10-30 22:21:24 +1100

    normalising indels

--- count_indels.py
+++ count_indels.py
@@ -8,7 +8,29 @@ import sys
 import cyvcf2
 import intervaltree
 
-def main(bed):
+def rotate(r):
+  '''
+    normalize repeats to start from earliest base e.g. GCAA -> AAGC
+  '''
+  best = 0
+  for idx, el in enumerate(r):
+    if r[idx] < r[best]:
+      best = idx
+
+  return r[best:] + r[:best]
+
+def complement(r):
+  rc = reverse_complement(r)
+  if r <= rc:
+    return r
+  else:
+    return rc
+
+RC = {'a': 't', 'c': 'g', 't': 'a', 'g': 'c', 'n': 'n', 'A': 'T', 'C': 'G', 'T': 'A', 'G': 'C', 'N': 'N'}
+def reverse_complement(repeat):
+  return ''.join([RC[x] for x in repeat][::-1])
+
+def main(bed, comp, rot):
   logging.info('parsing {}...'.format(bed))
 
   tree = {}
@@ -43,6 +65,10 @@ def main(bed):
         indel_len = len(variant.ALT[0].replace('-', '')) - len(variant.REF.replace('-', '')) # maf uses -
         # if we want only indels can filter on len here
         context = annotations['repeat']
+        if comp:
+          context = complement(context)
+        if rot:
+          context = rotate(context)
 
         counts['{}{}'.format(context, indel_len)] += 1
         
@@ -62,10 +88,12 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='mutational signature counter')
   parser.add_argument('--annotation', required=True, help='bed')
   parser.add_argument('--verbose', action='store_true', help='more logging')
+  parser.add_argument('--complement', action='store_true', help='ignore direction')
+  parser.add_argument('--rotate', action='store_true', help='merge same repeat types')
   args = parser.parse_args()
   if args.verbose:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
   else:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
-  main(args.annotation)
+  main(args.annotation, args.complement, args.rotate)
