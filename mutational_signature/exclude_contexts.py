8f1deb6ef977eba415f8ea19c6f75aabfe050816
Peter Georgeson <peter@supernifty.org>
2019-04-16 14:04:29 +1000

    show effect of context exclusion

--- mutational_signature/exclude_contexts.py
+++ mutational_signature/exclude_contexts.py
@@ -14,14 +14,21 @@ def exclude(contexts):
   sys.stderr.write('excluding {}\n'.format(contexts))
   header = sys.stdin.readline().strip('\n').split('\t')
   sys.stdout.write('{}\n'.format('\t'.join([x for x in header if x not in contexts])))
+  effects = {}
   for line in sys.stdin:
     cols = line.strip('\n').split('\t')
     sig = cols[0]
     vals = [float(x) for i, x in enumerate(cols[1:]) if header[i + 1] not in contexts]
-    sys.stderr.write('sig {} now has {}\n'.format(sig, len(vals)))
+    effect = sum([float(x) for i, x in enumerate(cols[1:]) if header[i + 1] in contexts])
+    sys.stderr.write('signature {} now has {} contexts. Effect: {:.2f}\n'.format(sig, len(vals), effect))
+    effects[sig] = effect
     new_sum = sum(vals) # normalise
     vals = [x / new_sum for x in vals]
     sys.stdout.write('{}\t{}\n'.format(sig, '\t'.join([str(x) for x in vals])))
+
+  # sorted effects
+  for item in [(k, effects[k]) for k in sorted(effects, key=effects.get, reverse=True)]:
+    sys.stderr.write('{}\t{:.2f}\n'.format(item[0], item[1]))
 
 if __name__ == '__main__':
   exclude(sys.argv[1:])
